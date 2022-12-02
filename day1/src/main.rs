a782ea1c19fa2cfaf5227ae96e90dcce7e12be15
Sven Groot <sgroot@gmail.com>
2022-12-02 15:53:21 -0800

    Iterator version of day 1, just to prove it's possible.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -10,17 +10,18 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
-    let mut result = Vec::new();
-    let mut sum = 0;
-    for line in reader.lines() {
-        let line = line?;
-        if line.len() == 0 {
-            result.push(sum);
-            sum = 0;
-        } else {
-            sum += u32::from_str(&line)?;
-        }
-    }
+    let mut result = reader
+        .lines()
+        .map(|l| l.map_err(Into::into))
+        .collect::<Result<Vec<_>>>()?
+        .split(|l| l.len() == 0)
+        .map(|split| {
+            split
+                .iter()
+                .map(|l| u32::from_str(l).map_err(Into::into))
+                .sum()
+        })
+        .collect::<Result<Vec<u32>>>()?;
 
     result.sort_by(|a, b| b.cmp(a));
     let sum: u32 = result.iter().take(3).sum();
