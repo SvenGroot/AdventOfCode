54523555900f608d926c4f06cb97b1a4f61807eb
Sven Groot <sgroot@gmail.com>
2022-12-02 15:57:51 -0800

    Improved error handling.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,6 +2,7 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    num::ParseIntError,
     str::FromStr,
 };
 
@@ -12,16 +13,11 @@ fn main() -> Result<()> {
     let reader = BufReader::new(file);
     let mut result = reader
         .lines()
-        .map(|l| l.map_err(Into::into))
-        .collect::<Result<Vec<_>>>()?
+        .map(|l| l)
+        .collect::<std::io::Result<Vec<_>>>()?
         .split(|l| l.len() == 0)
-        .map(|split| {
-            split
-                .iter()
-                .map(|l| u32::from_str(l).map_err(Into::into))
-                .sum()
-        })
-        .collect::<Result<Vec<u32>>>()?;
+        .map(|split| split.iter().map(|l| u32::from_str(l)).sum())
+        .collect::<Result<Vec<u32>, ParseIntError>>()?;
 
     result.sort_by(|a, b| b.cmp(a));
     let sum: u32 = result.iter().take(3).sum();
