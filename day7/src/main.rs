cde5735d9c588fdec306edbc2963757931fb4af1
Sven Groot <sgroot@gmail.com>
2022-12-07 12:56:41 -0800

    Day 7.

---
+++ day7/src/main.rs
@@ -0,0 +1,144 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+mod tree;
+
+use aoc::get_input;
+use std::{fmt::Display, str::FromStr};
+use tree::TreeNode;
+
+fn main() {
+    const PATH: &str = "input/day7.txt";
+    println!("Part 1: {}", part1(PATH));
+    println!("Part 2: {}", part2(PATH));
+}
+
+fn part1(path: &str) -> usize {
+    let mut tree = parse_input(path);
+    println!("{}", tree);
+    calc_dir_sizes(&mut tree);
+    let mut sum = 0;
+    tree.walk(&mut |f| {
+        if f.is_dir && f.size < 100000 {
+            sum += f.size
+        }
+    });
+
+    sum
+}
+
+fn part2(path: &str) -> usize {
+    let mut tree = parse_input(path);
+    calc_dir_sizes(&mut tree);
+    const TOTAL_SIZE: usize = 70000000;
+    const NEEDED_SPACE: usize = 30000000;
+
+    let free_space = TOTAL_SIZE - tree.content().size;
+    let additional_needed = NEEDED_SPACE - free_space;
+    let mut min = usize::MAX;
+    tree.walk(&mut |f| {
+        if f.is_dir && f.size > additional_needed && f.size < min {
+            min = f.size;
+        }
+    });
+
+    min
+}
+
+struct FileSystemItem {
+    is_dir: bool,
+    name: String,
+    size: usize,
+}
+
+impl FileSystemItem {
+    fn dir(name: String) -> Self {
+        Self {
+            is_dir: true,
+            name,
+            size: 0,
+        }
+    }
+
+    fn file(name: String, size: usize) -> Self {
+        Self {
+            is_dir: false,
+            name,
+            size,
+        }
+    }
+}
+
+impl Display for FileSystemItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_dir {
+            write!(f, "{} (dir)", self.name)?;
+        } else {
+            write!(f, "{} (file, size={})", self.name, self.size)?;
+        }
+
+        Ok(())
+    }
+}
+
+fn parse_input(path: &str) -> TreeNode<FileSystemItem> {
+    let mut root = TreeNode::new(FileSystemItem::dir("/".into()));
+    let mut iter = get_input(path);
+    parse_node(&mut root, &mut iter);
+
+    root
+}
+
+fn parse_node(
+    node: &mut TreeNode<FileSystemItem>,
+    iter: &mut impl Iterator<Item = String>,
+) -> Option<()> {
+    loop {
+        let line = iter.next()?;
+        if line.starts_with("$ cd ") {
+            let dir = &line[5..];
+
+            // "/" only occurs at the start of the input so we don;t actually need to handle it.
+            if dir == ".." {
+                break;
+            } else if dir != "/" {
+                let child = node.find_mut(|item| item.name == dir).unwrap();
+                parse_node(child, iter)?;
+            }
+        } else if line != "$ ls" {
+            let (size, name) = line.split_once(' ').unwrap();
+            if size == "dir" {
+                node.add_child(FileSystemItem::dir(name.into()));
+            } else {
+                let size = usize::from_str(size).unwrap();
+                node.add_child(FileSystemItem::file(name.into(), size));
+            }
+        }
+    }
+
+    Some(())
+}
+
+fn calc_dir_sizes(node: &mut TreeNode<FileSystemItem>) -> usize {
+    if node.content().is_dir {
+        node.content_mut().size = node.children_mut().map(calc_dir_sizes).sum();
+    }
+
+    node.content().size
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PATH: &str = "../input/sample/day7.txt";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(95437, part1(PATH));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(24933642, part2(PATH));
+    }
+}
---
+++ day7/src/tree.rs
@@ -0,0 +1,98 @@
+#![allow(dead_code)]
+
+use std::fmt::Display;
+
+// There are existing tree crates; I wanted to try building one.
+pub struct TreeNode<T> {
+    content: T,
+    children: Vec<TreeNode<T>>,
+}
+
+impl<T> TreeNode<T> {
+    pub fn new(content: T) -> Self {
+        Self {
+            content,
+            children: Vec::new(),
+        }
+    }
+
+    pub fn add_child(&mut self, content: T) -> &mut Self {
+        self.children.push(Self::new(content));
+        self.children.last_mut().unwrap()
+    }
+
+    pub fn children(&self) -> impl Iterator<Item = &Self> {
+        self.children.iter()
+    }
+
+    pub fn children_mut(&mut self) -> impl Iterator<Item = &mut Self> {
+        self.children.iter_mut()
+    }
+
+    pub fn walk(&self, f: &mut impl FnMut(&T)) {
+        f(&self.content);
+        for child in self.children.iter() {
+            child.walk(f);
+        }
+    }
+
+    pub fn walk_mut(&mut self, f: &mut impl FnMut(&mut T)) {
+        f(&mut self.content);
+        for child in self.children.iter_mut() {
+            child.walk_mut(f);
+        }
+    }
+
+    pub fn content(&self) -> &T {
+        &self.content
+    }
+
+    pub fn content_mut(&mut self) -> &mut T {
+        &mut self.content
+    }
+
+    pub fn find_mut(&mut self, mut pred: impl FnMut(&T) -> bool) -> Option<&mut Self> {
+        for child in self.children.iter_mut() {
+            if pred(&child.content) {
+                return Some(child);
+            }
+        }
+
+        None
+    }
+}
+
+impl<T: Display> TreeNode<T> {
+    fn print_helper(&self, indent: usize, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{:>indent$} {}", "-", &self.content, indent = indent)?;
+        for child in self.children() {
+            child.print_helper(indent + 2, f)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: Display> Display for TreeNode<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.print_helper(2, f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_node() {
+        let mut root = TreeNode::new(1);
+        let node = root.add_child(2);
+        let node2 = node.add_child(3);
+        root.add_child(4);
+        let mut sum = 0;
+        root.walk(&mut |val| sum += val);
+        assert_eq!(10, sum);
+        root.walk_mut(&mut |val| *val += 1);
+        assert_eq!(3, *(root.children().next().unwrap().content()));
+    }
+}
