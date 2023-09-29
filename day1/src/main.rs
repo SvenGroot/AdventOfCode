48870d38b9af5444eca3a45f68f21350dbd151d4
Sven Groot <svgroot@microsoft.com>
2023-09-28 18:31:03 -0700

    Restored parts 1 of day 1 and 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,45 @@
 // https://adventofcode.com/2022/day/1
-use std::str::FromStr;
+use std::{path::Path, str::FromStr};
 
-use aoc::get_input_vec;
+use aoc::{aoc_input, get_input_vec};
 
 fn main() {
-    let mut result = get_input_vec("input/day1.txt")
+    let path = aoc_input();
+    println!("Part 1: {}", part1(&path));
+    println!("Part 2: {}", part2(&path));
+}
+
+fn part1(path: impl AsRef<Path>) -> u32 {
+    get_input_vec(path)
+        .split(|l| l.is_empty())
+        .map(|split| split.iter().map(|l| u32::from_str(l).unwrap()).sum())
+        .max()
+        .unwrap()
+}
+
+fn part2(path: impl AsRef<Path>) -> u32 {
+    let mut result = get_input_vec(path)
         .split(|l| l.is_empty())
         .map(|split| split.iter().map(|l| u32::from_str(l).unwrap()).sum())
         .collect::<Vec<u32>>();
 
     result.sort_by(|a, b| b.cmp(a));
-    let sum: u32 = result.iter().take(3).sum();
-    println!("Sum: {}", sum);
+    result.iter().take(3).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use aoc::aoc_sample_input;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(24000, part1(aoc_sample_input()));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(45000, part2(aoc_sample_input()));
+    }
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,9 +1,35 @@
 // https://adventofcode.com/2022/day/2
 
-use aoc::get_input;
+use std::path::Path;
+
+use aoc::{aoc_input, get_input};
 
 fn main() {
-    let score: i32 = get_input("input/day2.txt")
+    let path = aoc_input();
+    println!("Part 1: {}", part1(&path));
+    println!("Part 2: {}", part2(&path));
+}
+
+fn part1(path: impl AsRef<Path>) -> i32 {
+    get_input(path)
+        .map(|line| {
+            let bytes = line.as_bytes();
+            let opponent_choice = bytes[0] as i32 - 'A' as i32;
+            let my_choice = bytes[2] as i32 - 'X' as i32;
+            let outcome = (my_choice - opponent_choice).rem_euclid(3);
+            let points = match outcome {
+                1 => 6,
+                0 => 3,
+                _ => 0,
+            };
+
+            points + my_choice + 1
+        })
+        .sum()
+}
+
+fn part2(path: impl AsRef<Path>) -> i32 {
+    get_input(path)
         .map(|line| {
             let opponent_choice = line.as_bytes()[0] as i32 - 'A' as i32;
             let outcome = line.as_bytes()[2] as i32 - 'Y' as i32;
@@ -16,7 +42,22 @@ fn main() {
                     _ => 0,
                 }
         })
-        .sum();
+        .sum()
+}
 
-    println!("Total score: {}", score);
+#[cfg(test)]
+mod tests {
+    use aoc::aoc_sample_input;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(15, part1(aoc_sample_input()));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(12, part2(aoc_sample_input()));
+    }
 }
