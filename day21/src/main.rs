e90d235cedda3d97227f9444e6e49ea30f7c9319
Sven Groot <svgroot@microsoft.com>
2023-09-28 17:34:07 -0700

    Day 21 part 2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -11,20 +11,57 @@ fn main() {
 
 fn part1(path: impl AsRef<Path>) -> isize {
     let monkeys: HashMap<_, _> = get_input(path).map(|line| Monkey::parse(&line)).collect();
-    monkeys["root"].get_number(&monkeys)
+    let result = monkeys["root"].get_number(&monkeys).unwrap();
+    result
 }
 
-fn part2(path: impl AsRef<Path>) -> usize {
-    get_input(path).map(|_| 0).sum()
+fn part2(path: impl AsRef<Path>) -> isize {
+    let mut monkeys: HashMap<_, _> = get_input(path).map(|line| Monkey::parse(&line)).collect();
+    monkeys.insert("humn".into(), Monkey(RefCell::new(MonkeyJob::Unknown)));
+    let root = &monkeys["root"];
+    let result = root.get_number(&monkeys);
+    assert!(result.is_none());
+    let job = root.0.borrow();
+    let (known, unknown) = match job.deref() {
+        MonkeyJob::Operation {
+            left: OperandValue::Number(known),
+            right: OperandValue::Unresolved(unknown),
+            ..
+        } => (*known, unknown),
+        MonkeyJob::Operation {
+            left: OperandValue::Unresolved(unknown),
+            right: OperandValue::Number(known),
+            ..
+        } => (*known, unknown),
+        _ => unreachable!(),
+    };
+
+    monkeys[unknown].resolve_unknown(&monkeys, known)
 }
 
 enum MonkeyJob {
     Number(isize),
     Operation {
-        left: String,
-        right: String,
+        left: OperandValue,
+        right: OperandValue,
         operator: char,
     },
+    Unknown,
+}
+
+#[derive(Clone)]
+enum OperandValue {
+    Number(isize),
+    Unresolved(String),
+}
+
+impl OperandValue {
+    fn get_number(&self, monkeys: &HashMap<String, Monkey>) -> Option<isize> {
+        match self {
+            OperandValue::Number(value) => Some(*value),
+            OperandValue::Unresolved(name) => monkeys[name].get_number(monkeys),
+        }
+    }
 }
 
 struct Monkey(RefCell<MonkeyJob>);
@@ -42,8 +79,8 @@ impl Monkey {
             (
                 name.into(),
                 Monkey(RefCell::new(MonkeyJob::Operation {
-                    left: splits[0].into(),
-                    right: splits[2].into(),
+                    left: OperandValue::Unresolved(splits[0].into()),
+                    right: OperandValue::Unresolved(splits[2].into()),
                     operator: splits[1].chars().next().unwrap(),
                 })),
             )
@@ -52,36 +89,86 @@ impl Monkey {
         }
     }
 
-    fn get_number(&self, monkeys: &HashMap<String, Monkey>) -> isize {
+    fn get_number(&self, monkeys: &HashMap<String, Monkey>) -> Option<isize> {
         let job = self.0.borrow();
         let (value, replace) = match job.deref() {
-            MonkeyJob::Number(value) => (*value, false),
+            MonkeyJob::Unknown => (None, None),
+            MonkeyJob::Number(value) => (Some(*value), None),
             MonkeyJob::Operation {
                 left,
                 right,
                 operator,
             } => {
-                let left = monkeys[left].get_number(monkeys);
-                let right = monkeys[right].get_number(monkeys);
-                (
-                    match *operator {
-                        '+' => left + right,
-                        '-' => left - right,
-                        '*' => left * right,
-                        '/' => left / right,
-                        _ => unreachable!(),
-                    },
-                    true,
-                )
+                let left_val = left.get_number(monkeys);
+                let right_val = right.get_number(monkeys);
+                match (left_val, right_val) {
+                    (Some(left), Some(right)) => {
+                        let result = match *operator {
+                            '+' => left + right,
+                            '-' => left - right,
+                            '*' => left * right,
+                            '/' => left / right,
+                            _ => unreachable!(),
+                        };
+                        (Some(result), Some(MonkeyJob::Number(result)))
+                    }
+                    (Some(left), None) => (
+                        None,
+                        Some(MonkeyJob::Operation {
+                            left: OperandValue::Number(left),
+                            right: right.clone(),
+                            operator: *operator,
+                        }),
+                    ),
+                    (None, Some(right)) => (
+                        None,
+                        Some(MonkeyJob::Operation {
+                            left: left.clone(),
+                            right: OperandValue::Number(right),
+                            operator: *operator,
+                        }),
+                    ),
+                    _ => unreachable!(),
+                }
             }
         };
 
         drop(job);
-        if replace {
-            *self.0.borrow_mut() = MonkeyJob::Number(value);
+        if let Some(new_value) = replace {
+            *self.0.borrow_mut() = new_value
         }
 
         value
+    }
+
+    fn resolve_unknown(&self, monkeys: &HashMap<String, Monkey>, expected: isize) -> isize {
+        let job = self.0.borrow();
+        let (known, unknown, is_left, operator) = match job.deref() {
+            MonkeyJob::Operation {
+                left: OperandValue::Number(known),
+                right: OperandValue::Unresolved(unknown),
+                operator,
+            } => (*known, unknown, false, *operator),
+            MonkeyJob::Operation {
+                left: OperandValue::Unresolved(unknown),
+                right: OperandValue::Number(known),
+                operator,
+            } => (*known, unknown, true, *operator),
+            MonkeyJob::Unknown => return expected,
+            _ => unreachable!(),
+        };
+
+        let x = match (operator, is_left) {
+            ('+', _) => expected - known, // known + x = expected or x + num = expected
+            ('-', false) => known - expected, // known - x = expected
+            ('-', true) => expected + known, // x - known = expected
+            ('*', _) => expected / known, // known * x = expected or reverse
+            ('/', false) => known / expected, // known / x = expected
+            ('/', true) => known * expected, // x / known = expected
+            _ => unreachable!(),
+        };
+
+        monkeys[unknown].resolve_unknown(monkeys, x)
     }
 }
 
@@ -98,6 +185,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(aoc_sample_input()));
+        assert_eq!(301, part2(aoc_sample_input()));
     }
 }
