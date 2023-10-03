7f8c1a0292fdfe481c3071bc8fe1acd9837ca4fb
Sven Groot <svgroot@microsoft.com>
2023-10-02 17:25:18 -0700

    Day 22 part 2

--- aoc/src/grid.rs
+++ aoc/src/grid.rs
@@ -2,7 +2,7 @@ use std::{
     convert::TryFrom,
     fmt::Display,
     num::{NonZeroUsize, ParseIntError, TryFromIntError},
-    ops::{Add, AddAssign, Index, IndexMut, Sub, SubAssign},
+    ops::{Add, AddAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign},
     path::Path,
     str::FromStr,
 };
@@ -292,6 +292,38 @@ impl SubAssign for Point {
     }
 }
 
+impl Mul<usize> for Point {
+    type Output = Point;
+
+    fn mul(self, rhs: usize) -> Self::Output {
+        Self {
+            row: self.row * rhs,
+            col: self.col * rhs,
+        }
+    }
+}
+
+impl MulAssign<usize> for Point {
+    fn mul_assign(&mut self, rhs: usize) {
+        self.row *= rhs;
+        self.col *= rhs;
+    }
+}
+
+impl Add<PointDiff> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: PointDiff) -> Self::Output {
+        self.add_diff(rhs).unwrap()
+    }
+}
+
+impl AddAssign<PointDiff> for Point {
+    fn add_assign(&mut self, rhs: PointDiff) {
+        *self = self.add_diff(rhs).unwrap();
+    }
+}
+
 impl TryFrom<PointDiff> for Point {
     type Error = TryFromIntError;
 
@@ -300,6 +332,15 @@ impl TryFrom<PointDiff> for Point {
             row: value.row.try_into()?,
             col: value.col.try_into()?,
         })
+    }
+}
+
+impl From<(usize, usize)> for Point {
+    fn from(value: (usize, usize)) -> Self {
+        Self {
+            row: value.0,
+            col: value.1,
+        }
     }
 }
 
@@ -382,6 +423,10 @@ impl PointDiff {
             Rotation::Left => Self::new(-self.col, self.row),
         }
     }
+
+    pub fn invert(&self) -> Self {
+        Self::new(-self.row, -self.col)
+    }
 }
 
 impl TryFrom<Point> for PointDiff {
@@ -435,6 +480,55 @@ impl SubAssign for PointDiff {
 pub enum Rotation {
     Left,
     Right,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+impl Rectangle {
+    pub fn new(top_left: Point, bottom_right: Point) -> Self {
+        assert!(bottom_right.row >= top_left.row && bottom_right.col >= top_left.col);
+        Self {
+            top_left,
+            bottom_right,
+        }
+    }
+
+    pub fn from_size(top_left: Point, width: NonZeroUsize, height: NonZeroUsize) -> Self {
+        Self {
+            top_left,
+            bottom_right: Point::new(
+                top_left.row + height.get() - 1,
+                top_left.col + width.get() - 1,
+            ),
+        }
+    }
+
+    pub fn top_left(&self) -> Point {
+        self.top_left
+    }
+
+    pub fn bottom_right(&self) -> Point {
+        self.bottom_right
+    }
+
+    pub fn width(&self) -> usize {
+        self.bottom_right.col - self.top_left.col + 1
+    }
+
+    pub fn height(&self) -> usize {
+        self.bottom_right.row - self.top_left.row + 1
+    }
+
+    pub fn contains(&self, point: Point) -> bool {
+        self.top_left.row <= point.row
+            && self.top_left.col <= point.col
+            && point.row <= self.bottom_right.row
+            && point.col <= self.bottom_right.col
+    }
 }
 
 pub struct Neighbors {
--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,9 +1,9 @@
 // https://adventofcode.com/2022/day/22
-use std::{fmt::Display, path::Path};
+use std::{fmt::Display, num::NonZeroUsize, path::Path, str::FromStr};
 
 use aoc::{
-    aoc_input, get_input,
-    grid::{grid_from_non_uniform_lines, Grid, Point, PointDiff, Rotation},
+    aoc_input, get_input, get_input_vec,
+    grid::{grid_from_non_uniform_lines, Grid, Point, PointDiff, Rectangle, Rotation},
     iterator::PeekableExt,
 };
 
@@ -13,6 +13,8 @@ fn main() {
     println!("Part 2: {}", part2(&path));
 }
 
+// Walk the grid using the path given, stopping at walls, wrapping to the other side if you walk off
+// the board.
 fn part1(path: impl AsRef<Path>) -> usize {
     let (mut board, moves) = load_input(path);
     for mv in moves {
@@ -20,19 +22,25 @@ fn part1(path: impl AsRef<Path>) -> usize {
     }
 
     println!("{}", board.grid);
-    let facing = match board.current_dir {
-        PointDiff::RIGHT => 0,
-        PointDiff::DOWN => 1,
-        PointDiff::LEFT => 2,
-        PointDiff::UP => 3,
-        _ => unreachable!(),
-    };
-
-    1000 * (board.current_pos.row() + 1) + 4 * (board.current_pos.col() + 1) + facing
+    board.get_password()
 }
 
+// Same as part1, but fold the board into a cube, and continue on the adjacent face when you walk
+// off the board.
 fn part2(path: impl AsRef<Path>) -> usize {
-    get_input(path).map(|_| 0).sum()
+    let path = path.as_ref();
+    let (mut board, moves) = load_input(path);
+
+    // Extra file containing cube faces and face transitions; not part of original input. See
+    // readme.
+    let cube_path = path.with_file_name("day22_cube.txt");
+    board.cube = Some(Cube::from_file(cube_path));
+    for mv in moves {
+        board.do_move(&mv);
+    }
+
+    println!("{}", board.grid);
+    board.get_password()
 }
 
 fn load_input(path: impl AsRef<Path>) -> (Board, Vec<Move>) {
@@ -79,6 +87,7 @@ fn load_input(path: impl AsRef<Path>) -> (Board, Vec<Move>) {
             grid,
             current_pos: start,
             current_dir: PointDiff::RIGHT,
+            cube: None,
         },
         moves,
     )
@@ -108,6 +117,7 @@ struct Board {
     grid: Grid<Tile>,
     current_pos: Point,
     current_dir: PointDiff,
+    cube: Option<Cube>,
 }
 
 impl Board {
@@ -121,25 +131,42 @@ impl Board {
     fn walk(&mut self, count: u32) {
         for _ in 0..count {
             let new_pos = self.grid.add_point(self.current_pos, self.current_dir);
-            let new_pos = match new_pos {
+            let (new_pos, new_dir) = match new_pos {
                 None => self.wrap_pos(),
                 Some(pt) if matches!(self.grid[pt], Tile::Blank) => self.wrap_pos(),
-                Some(pt) => pt,
+                Some(pt) => (pt, self.current_dir),
             };
 
             if matches!(self.grid[new_pos], Tile::Wall) {
                 break;
             }
 
-            self.grid[new_pos] = Tile::Open(Some(self.current_dir));
             self.current_pos = new_pos;
+            self.current_dir = new_dir;
+            self.grid[new_pos] = Tile::Open(Some(self.current_dir));
         }
     }
 
-    fn wrap_pos(&self) -> Point {
+    fn get_password(&self) -> usize {
+        let facing = match self.current_dir {
+            PointDiff::RIGHT => 0,
+            PointDiff::DOWN => 1,
+            PointDiff::LEFT => 2,
+            PointDiff::UP => 3,
+            _ => unreachable!(),
+        };
+
+        1000 * (self.current_pos.row() + 1) + 4 * (self.current_pos.col() + 1) + facing
+    }
+
+    fn wrap_pos(&self) -> (Point, PointDiff) {
+        if let Some(cube) = &self.cube {
+            return cube.do_transition(self.current_pos, self.current_dir);
+        }
+
         let predicate = |tile: &Tile| !matches!(tile, Tile::Blank);
 
-        match self.current_dir {
+        let new_pos = match self.current_dir {
             PointDiff::RIGHT => self
                 .grid
                 .find_in_row(self.current_pos.row(), predicate)
@@ -157,7 +184,9 @@ impl Board {
                 .find_in_col(self.current_pos.col(), predicate)
                 .unwrap(),
             _ => unreachable!(),
-        }
+        };
+
+        (new_pos, self.current_dir)
     }
 
     fn turn(&mut self, rotation: Rotation) {
@@ -169,6 +198,161 @@ impl Board {
 enum Move {
     Walk(u32),
     Turn(Rotation),
+}
+
+#[derive(Debug, Clone)]
+struct FaceTransition {
+    from_face: usize,
+    dir: PointDiff,
+    new_face: usize,
+    new_dir: PointDiff,
+    reverse: bool,
+}
+
+impl FaceTransition {
+    fn invert(&self) -> Self {
+        Self {
+            from_face: self.new_face,
+            dir: self.new_dir.invert(),
+            new_face: self.from_face,
+            new_dir: self.dir.invert(),
+            reverse: self.reverse,
+        }
+    }
+}
+
+impl FromStr for FaceTransition {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let splits: Vec<_> = s.split(',').collect();
+        let bytes = splits[0].as_bytes();
+        let from_face = bytes[0] - b'1';
+        let dir = match bytes[1] {
+            b'T' => PointDiff::UP,
+            b'R' => PointDiff::RIGHT,
+            b'B' => PointDiff::DOWN,
+            b'L' => PointDiff::LEFT,
+            _ => unreachable!(),
+        };
+
+        let bytes = splits[1].as_bytes();
+        let new_face = bytes[0] - b'1';
+        let new_dir = match bytes[1] {
+            b'T' => PointDiff::DOWN,
+            b'R' => PointDiff::LEFT,
+            b'B' => PointDiff::UP,
+            b'L' => PointDiff::RIGHT,
+            _ => unreachable!(),
+        };
+
+        let reverse = splits[2] == "true";
+        Ok(Self {
+            from_face: from_face.into(),
+            dir,
+            new_face: new_face.into(),
+            new_dir,
+            reverse,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Cube {
+    faces: [Rectangle; 6],
+    transitions: Vec<FaceTransition>,
+}
+
+impl Cube {
+    fn from_file(path: impl AsRef<Path>) -> Self {
+        let input = get_input_vec(path);
+        let face_size = input[0].parse().unwrap();
+        let layout: Vec<_> = input[1]
+            .split(';')
+            .map(|pos| Point::from_str(pos).unwrap())
+            .collect();
+
+        let faces = Cube::from_grid(layout.try_into().unwrap(), face_size);
+        let mut transitions: Vec<_> = input[2..]
+            .iter()
+            .map(|line| FaceTransition::from_str(line).unwrap())
+            .collect();
+
+        transitions.extend(transitions.clone().iter().map(FaceTransition::invert));
+
+        Self { faces, transitions }
+    }
+
+    fn from_grid(layout: [Point; 6], face_size: NonZeroUsize) -> [Rectangle; 6] {
+        layout.map(|face| Rectangle::from_size(face * face_size.get(), face_size, face_size))
+    }
+
+    fn do_transition(&self, pos: Point, dir: PointDiff) -> (Point, PointDiff) {
+        let from_face = self.get_face(pos);
+        let transition = self
+            .transitions
+            .iter()
+            .find(|t| t.from_face == from_face && t.dir == dir)
+            .unwrap_or_else(|| panic!("Should have {from_face} and {dir:?} (pos = {pos:?})"));
+
+        let from_face = &self.faces[from_face];
+        let offset = pos - from_face.top_left();
+
+        // If we are moving off a side, we need to translate the row to the new face, otherwise the
+        // column.
+        let source_pos = if Self::is_side(dir) {
+            offset.row()
+        } else {
+            offset.col()
+        };
+
+        // If we are entering the new face on a side, the column will be either the left or right
+        // edge of the new face (depending on the direction), while the row will use the translated
+        // source value. This is done either from the start or end of the edge depending on the
+        // reverse value.
+        let new_face = &self.faces[transition.new_face];
+        let new_pos = if Self::is_side(transition.new_dir) {
+            let new_col = if transition.new_dir == PointDiff::RIGHT {
+                new_face.top_left().col()
+            } else {
+                new_face.bottom_right().col()
+            };
+
+            let new_row = if transition.reverse {
+                new_face.bottom_right().row() - source_pos
+            } else {
+                new_face.top_left().row() + source_pos
+            };
+
+            Point::new(new_row, new_col)
+        } else {
+            let new_row = if transition.new_dir == PointDiff::DOWN {
+                new_face.top_left().row()
+            } else {
+                new_face.bottom_right().row()
+            };
+
+            let new_col = if transition.reverse {
+                new_face.bottom_right().col() - source_pos
+            } else {
+                new_face.top_left().col() + source_pos
+            };
+
+            Point::new(new_row, new_col)
+        };
+
+        (new_pos, transition.new_dir)
+    }
+
+    fn get_face(&self, pos: Point) -> usize {
+        self.faces
+            .iter()
+            .position(|face| face.contains(pos))
+            .unwrap_or_else(|| panic!("No face for {pos:?}"))
+    }
+
+    fn is_side(dir: PointDiff) -> bool {
+        dir == PointDiff::LEFT || dir == PointDiff::RIGHT
+    }
 }
 
 #[cfg(test)]
@@ -184,6 +368,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(aoc_sample_input()));
+        assert_eq!(5031, part2(aoc_sample_input()));
     }
 }
