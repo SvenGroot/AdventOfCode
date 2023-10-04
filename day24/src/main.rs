ae053e7258349300ca0b09f48fe9ab530a5edb0d
Sven Groot <svgroot@microsoft.com>
2023-10-03 18:29:05 -0700

    Day 22 WIP

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -14,13 +14,16 @@ fn main() {
 }
 
 fn part1(path: impl AsRef<Path>) -> usize {
-    let mut valley = Valley::from_file(path);
-    println!("{}", valley.0);
-    for _ in 0..5 {
-        valley.move_blizzards();
-        println!("{}", valley.0);
+    let valley = Valley::from_file(path);
+    let mut empty_valley = valley.clone();
+    for (_, tile) in empty_valley.0.cells_mut() {
+        if let Tile::Floor(blizzards) = tile {
+            *blizzards = Default::default();
+        }
     }
-    0
+
+    let mut best = 30;
+    Simulation::new(valley).run(&empty_valley, &mut best)
 }
 
 fn part2(path: impl AsRef<Path>) -> usize {
@@ -32,6 +35,29 @@ enum Tile {
     Wall,
     // Bitfield of which directions are present.
     Floor(BitArray<[u8; 1]>),
+}
+
+impl Tile {
+    // fn get_blizzards(&self) -> &BitArray<[u8; 1]> {
+    //     match self {
+    //         Tile::Floor(blizzards) => blizzards,
+    //         _ => panic!("Not a floor."),
+    //     }
+    // }
+
+    fn get_blizzards_mut(&mut self) -> &mut BitArray<[u8; 1]> {
+        match self {
+            Tile::Floor(blizzards) => blizzards,
+            _ => panic!("Not a floor."),
+        }
+    }
+
+    fn is_clear(&self) -> bool {
+        match self {
+            Tile::Floor(blizzards) => !blizzards.any(),
+            Tile::Wall => false,
+        }
+    }
 }
 
 impl Display for Tile {
@@ -56,6 +82,7 @@ impl Display for Tile {
     }
 }
 
+#[derive(Clone)]
 struct Valley(Grid<Tile>);
 
 impl Valley {
@@ -81,7 +108,8 @@ impl Valley {
         )
     }
 
-    fn move_blizzards(&mut self) {
+    fn move_blizzards(&self, empty_valley: &Valley) -> Self {
+        let mut target = empty_valley.clone();
         for pos in self.0.bounding_rect().points() {
             if let Tile::Floor(blizzards) = self.0[pos] {
                 for i in 0..4 {
@@ -92,25 +120,15 @@ impl Valley {
                             new_pos = self.wrap_blizzard(pos, dir);
                         }
 
-                        let Tile::Floor(new_blizzards) = &mut self.0[new_pos] else {
-                            unreachable!();
-                        };
+                        let new_blizzards = target.0[new_pos].get_blizzards_mut();
 
-                        // Use upper 4 bits to store new positions.
-                        new_blizzards.set(i + 4, true);
+                        new_blizzards.set(i, true);
                     }
                 }
             }
         }
 
-        for (_, tile) in self.0.cells_mut() {
-            if let Tile::Floor(blizzards) = tile {
-                let values = blizzards.as_raw_mut_slice();
-
-                // Move pending blizzards into active position, and clear pending.
-                values[0] >>= 4;
-            }
-        }
+        target
     }
 
     fn wrap_blizzard(&self, pos: Point, dir: PointDiff) -> Point {
@@ -121,6 +139,74 @@ impl Valley {
             PointDiff::RIGHT => Point::new(pos.row(), 1),
             _ => unreachable!(),
         }
+    }
+}
+
+struct Simulation {
+    valley: Valley,
+    player: Point,
+    wait_count: usize,
+    minute: usize,
+}
+
+impl Simulation {
+    fn new(valley: Valley) -> Self {
+        Self {
+            valley,
+            player: Point::new(0, 1),
+            wait_count: 0,
+            minute: 0,
+        }
+    }
+
+    fn run(self, empty_valley: &Valley, best: &mut usize) -> usize {
+        if self.minute >= *best {
+            return usize::MAX;
+        }
+
+        let new_valley = self.valley.move_blizzards(empty_valley);
+
+        // Check if we can move to the tile adjacent to the exit tile.
+        if (self.player == Point::new(self.valley.0.height() - 3, self.valley.0.width() - 2)
+            || self.player == Point::new(self.valley.0.height() - 2, self.valley.0.width() - 3))
+            && new_valley.0[Point::new(self.valley.0.height() - 2, self.valley.0.width() - 2)]
+                .is_clear()
+        {
+            *best = self.minute;
+            println!("New best {best}");
+            return self.minute + 2;
+        }
+
+        // Check the possible moves.
+        // N.B. Can't walk off the grid thanks to walls, so skip that check.
+        let mut min_minutes = usize::MAX;
+        for nb in self.player.straight_neighbors() {
+            if nb != Point::new(0, 1) && new_valley.0[nb].is_clear() {
+                let next = Simulation {
+                    valley: new_valley.clone(),
+                    player: nb,
+                    wait_count: 0,
+                    minute: self.minute + 1,
+                };
+
+                min_minutes = next.run(empty_valley, best).min(min_minutes);
+            }
+        }
+
+        // Check if we can wait.
+        let max_wait = self.valley.0.width().max(self.valley.0.height()) - 2;
+        if new_valley.0[self.player].is_clear() && self.wait_count < max_wait {
+            let next = Simulation {
+                valley: new_valley,
+                player: self.player,
+                wait_count: self.wait_count + 1,
+                minute: self.minute + 1,
+            };
+
+            min_minutes = next.run(empty_valley, best).min(min_minutes);
+        }
+
+        min_minutes
     }
 }
 
