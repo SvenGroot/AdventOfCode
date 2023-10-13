78bd56f311ddfbb09b05aff07b5dc4576643f493
Sven Groot <sgroot@gmail.com>
2023-10-13 10:47:56 -0700

    2021 day 10 part 2

--- 2021/day10/src/main.rs
+++ 2021/day10/src/main.rs
@@ -1,6 +1,8 @@
 // https://adventofcode.com/2021/day/10
 
-use aoc::input::AocInput;
+use std::str::FromStr;
+
+use aoc::{input::AocInput, iterator::IntoVec};
 
 fn main() {
     println!("Part 1: {}", part1(AocInput::from_input()));
@@ -9,40 +11,85 @@ fn main() {
 
 fn part1(input: AocInput) -> usize {
     input
-        .filter_map(|s| find_illegal_character(s).map(get_score))
+        .parsed::<ParsedLine>()
+        .filter_map(|line| line.get_illegal_character().map(get_score))
         .sum()
 }
 
 fn part2(input: AocInput) -> usize {
-    input.map(|_| 0).sum()
+    let mut scores = input
+        .parsed::<ParsedLine>()
+        .filter_map(|line| line.get_autocomplete_score())
+        .into_vec();
+
+    scores.sort();
+    scores[(scores.len() - 1) / 2]
 }
 
-fn find_illegal_character(s: String) -> Option<u8> {
-    let mut expected = Vec::new();
-    for ch in s.bytes() {
-        let close = match ch {
-            b'(' => b')',
-            b'[' => b']',
-            b'{' => b'}',
-            b'<' => b'>',
-            _ => {
-                if let Some(expected) = expected.pop() {
-                    if expected != ch {
-                        return Some(ch);
-                    }
+enum ParsedLine {
+    Corrupt(u8),
+    Incomplete(Vec<u8>),
+}
 
-                    continue;
-                } else {
-                    // Incomplete line
-                    return None;
-                }
-            }
-        };
-
-        expected.push(close);
+impl ParsedLine {
+    fn get_illegal_character(&self) -> Option<u8> {
+        match self {
+            ParsedLine::Corrupt(ch) => Some(*ch),
+            _ => None,
+        }
     }
 
-    None
+    fn get_autocomplete_score(self) -> Option<usize> {
+        let ParsedLine::Incomplete(mut expected) = self else {
+            return None;
+        };
+
+        let mut score = 0;
+        while let Some(next) = expected.pop() {
+            let char_score = match next {
+                b')' => 1,
+                b']' => 2,
+                b'}' => 3,
+                b'>' => 4,
+                _ => unreachable!(),
+            };
+
+            score = score * 5 + char_score;
+        }
+
+        Some(score)
+    }
+}
+
+impl FromStr for ParsedLine {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut expected = Vec::new();
+        for ch in s.bytes() {
+            let close = match ch {
+                b'(' => b')',
+                b'[' => b']',
+                b'{' => b'}',
+                b'<' => b'>',
+                _ => {
+                    if let Some(expected) = expected.pop() {
+                        if expected != ch {
+                            return Ok(ParsedLine::Corrupt(ch));
+                        }
+
+                        continue;
+                    } else {
+                        return Ok(ParsedLine::Incomplete(expected));
+                    }
+                }
+            };
+
+            expected.push(close);
+        }
+
+        Ok(ParsedLine::Incomplete(expected))
+    }
 }
 
 fn get_score(ch: u8) -> usize {
@@ -66,6 +113,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(AocInput::from_sample()));
+        assert_eq!(288957, part2(AocInput::from_sample()));
     }
 }
