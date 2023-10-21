dc244dd0f6a3517de7f11f0441c83d39b0b93684
Sven Groot <sgroot@gmail.com>
2023-10-20 17:55:32 -0700

    2021 day 23 part 2

--- 2021/day23/README.md
+++ 2021/day23/README.md
@@ -1,3 +1,7 @@
 # Day 23
 
 [Puzzle description.](https://adventofcode.com/2021/day/23)
+
+Pretty neat, but happily surprised that the straight-forward approach actually worked: if you can
+move into a final position, do so, otherwise just try every move. In release mode, it's fast enough
+even for part 2 with no memoization or extra tricks needed.
--- 2021/day23/src/main.rs
+++ 2021/day23/src/main.rs
@@ -9,21 +9,26 @@ fn main() {
     println!("Part 2: {}", part2(AocInput::from_input()));
 }
 
+// Get the minimal energy needed to organize the amphipods.
 fn part1(input: AocInput) -> usize {
-    let burrow = Burrow::from_input(input);
+    let burrow = Burrow::<8>::from_input(input);
     burrow.do_move()
 }
 
+// Same, with some extra amphipods added in.
+// WARNING: Slow in debug mode.
 fn part2(input: AocInput) -> usize {
-    input.map(|_| 0).sum()
+    let burrow = Burrow::<16>::from_input(input);
+    burrow.do_move()
 }
 
-#[derive(Default, Clone)]
-struct Burrow {
-    amphipods: [Amphipod; 8],
+#[derive(Clone)]
+struct Burrow<const C: usize> {
+    amphipods: [Amphipod; C],
+    room_size: u8,
 }
 
-impl Burrow {
+impl<const C: usize> Burrow<C> {
     fn from_input(mut input: AocInput) -> Self {
         let mut amphipods = Vec::new();
         input.next().unwrap();
@@ -38,16 +43,56 @@ impl Burrow {
         }
 
         let line = input.next().unwrap();
+        let subroom = if C == 16 { 3 } else { 1 };
         for i in 0..4 {
             let amphipod = line.as_bytes()[3 + 2 * i];
             amphipods.push(Amphipod {
-                pos: AmphipodPos::Room(i as u8, 1),
+                pos: AmphipodPos::Room(i as u8, subroom),
                 kind: amphipod as char,
             });
         }
 
+        // Add in the extra amphipods (this is the same for the sample and real input)
+        if C == 16 {
+            amphipods.extend_from_slice(&[
+                Amphipod {
+                    pos: AmphipodPos::Room(0, 1),
+                    kind: 'D',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(0, 2),
+                    kind: 'D',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(1, 1),
+                    kind: 'C',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(1, 2),
+                    kind: 'B',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(2, 1),
+                    kind: 'B',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(2, 2),
+                    kind: 'A',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(3, 1),
+                    kind: 'A',
+                },
+                Amphipod {
+                    pos: AmphipodPos::Room(3, 2),
+                    kind: 'C',
+                },
+            ]);
+        }
+
         Self {
             amphipods: amphipods.try_into().unwrap(),
+            room_size: (C / 4) as u8,
         }
     }
 
@@ -75,11 +120,11 @@ impl Burrow {
         let mut final_count = 0;
         // Check if any amphipod can move to its final destination. If so, that is the move we do.
         for (index, a) in self.amphipods.iter().enumerate() {
-            if !a.is_final(&self) {
+            if a.current_room() != Some(a.target_room()) {
                 if let Some(pos) = self.room_available(a.target_room()) {
                     has_move |= self.do_next_move(pos, index, &mut min_energy);
                 }
-            } else {
+            } else if a.is_final(&self) {
                 final_count += 1;
             }
         }
@@ -134,19 +179,17 @@ impl Burrow {
     }
 
     fn room_available(&self, room: u8) -> Option<AmphipodPos> {
-        if !self.pos_free(&AmphipodPos::Room(room, 0)) {
-            return None;
-        }
-
-        if let Some(a) = self.get_pos(&AmphipodPos::Room(room, 1)) {
-            if a.target_room() != room {
-                return None;
+        for subroom in (0..self.room_size).rev() {
+            if let Some(a) = self.get_pos(&AmphipodPos::Room(room, subroom)) {
+                if a.target_room() != room {
+                    break;
+                }
+            } else {
+                return Some(AmphipodPos::Room(room, subroom));
             }
-
-            return Some(AmphipodPos::Room(room, 0));
         }
 
-        Some(AmphipodPos::Room(room, 1))
+        None
     }
 
     fn pos_free(&self, pos: &AmphipodPos) -> bool {
@@ -158,36 +201,8 @@ impl Burrow {
     }
 
     fn path_free(&self, from: &AmphipodPos, to: &AmphipodPos) -> Option<usize> {
-        let mut energy = 0;
-        let from = match from {
-            AmphipodPos::Room(room, pos) => {
-                energy += 1;
-                if *pos == 1 {
-                    energy += 1;
-                    if !self.pos_free(&AmphipodPos::Room(*room, 0)) {
-                        return None;
-                    }
-                }
-
-                room * 2 + 2
-            }
-            AmphipodPos::Hallway(pos) => *pos,
-        };
-
-        let to = match to {
-            AmphipodPos::Room(room, pos) => {
-                energy += 1;
-                if *pos == 1 {
-                    energy += 1;
-                    if !self.pos_free(&AmphipodPos::Room(*room, 0)) {
-                        return None;
-                    }
-                }
-
-                room * 2 + 2
-            }
-            AmphipodPos::Hallway(pos) => *pos,
-        };
+        let (from, from_energy) = self.pos_to_hallway_index(from)?;
+        let (to, to_energy) = self.pos_to_hallway_index(to)?;
 
         let source = from.min(to);
         let dest = from.max(to);
@@ -197,14 +212,34 @@ impl Burrow {
             }
         }
 
-        energy += dest - source;
+        let energy = dest - source + from_energy + to_energy;
         Some(energy as usize)
+    }
+
+    fn pos_to_hallway_index(&self, from: &AmphipodPos) -> Option<(u8, u8)> {
+        let mut energy = 0;
+        let hallway_pos = match from {
+            AmphipodPos::Room(room, pos) => {
+                energy += 1;
+                if *pos != 0 {
+                    energy += pos;
+                    if (0..*pos).any(|subroom| !self.pos_free(&AmphipodPos::Room(*room, subroom))) {
+                        return None;
+                    }
+                }
+
+                room * 2 + 2
+            }
+            AmphipodPos::Hallway(pos) => *pos,
+        };
+
+        Some((hallway_pos, energy))
     }
 
     const HALLWAY_LEN: u8 = 11;
 }
 
-impl Display for Burrow {
+impl<const C: usize> Display for Burrow<C> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "#############")?;
         write!(f, "#")?;
@@ -222,14 +257,16 @@ impl Display for Burrow {
             self.get_room_char(3, 0),
         )?;
 
-        writeln!(
-            f,
-            "  #{}#{}#{}#{}#",
-            self.get_room_char(0, 1),
-            self.get_room_char(1, 1),
-            self.get_room_char(2, 1),
-            self.get_room_char(3, 1),
-        )?;
+        for subroom in 1..self.room_size {
+            writeln!(
+                f,
+                "  #{}#{}#{}#{}#",
+                self.get_room_char(0, subroom),
+                self.get_room_char(1, subroom),
+                self.get_room_char(2, subroom),
+                self.get_room_char(3, subroom),
+            )?;
+        }
 
         writeln!(f, "  #########")
     }
@@ -246,15 +283,24 @@ impl Amphipod {
         self.kind as u8 - b'A'
     }
 
-    fn is_final(&self, burrow: &Burrow) -> bool {
+    fn current_room(&self) -> Option<u8> {
+        match &self.pos {
+            AmphipodPos::Room(room, _) => Some(*room),
+            AmphipodPos::Hallway(_) => None,
+        }
+    }
+
+    fn is_final<const C: usize>(&self, burrow: &Burrow<C>) -> bool {
         if let AmphipodPos::Room(room, pos) = self.pos {
             room == self.target_room()
-                && (pos == 1
-                    || burrow
-                        .get_pos(&AmphipodPos::Room(room, 1))
-                        .unwrap()
-                        .target_room()
-                        == room)
+                && (pos == burrow.room_size - 1
+                    || (pos + 1..burrow.room_size).all(|subroom| {
+                        burrow
+                            .get_pos(&AmphipodPos::Room(room, subroom))
+                            .unwrap()
+                            .target_room()
+                            == room
+                    }))
         } else {
             false
         }
@@ -294,6 +340,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(AocInput::from_sample()));
+        // WARNING: Slow in debug mode.
+        assert_eq!(44169, part2(AocInput::from_sample()));
     }
 }
