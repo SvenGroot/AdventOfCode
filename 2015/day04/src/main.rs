e7b59766044e80bbc7500c82354939edecaee426
Sven Groot <svgroot@microsoft.com>
2023-10-25 17:44:19 -0700

    2021 day 4 part 2

--- 2015/day04/src/main.rs
+++ 2015/day04/src/main.rs
@@ -1,7 +1,7 @@
 // https://adventofcode.com/2015/day/4
 
 use aoc::input::AocInput;
-use md5::{Digest, Md5};
+use md5::{digest::Output, Digest, Md5};
 
 fn main() {
     println!("Part 1: {}", part1(AocInput::from_input()));
@@ -10,11 +10,24 @@ fn main() {
 
 // Find a hash that starts with five zeroes.
 fn part1(input: AocInput) -> usize {
+    find_hash(input, |digest| {
+        digest[0] == 0 && digest[1] == 0 && digest[2] < 0x10
+    })
+}
+
+// Find a hash that starts with six zeroes.
+fn part2(input: AocInput) -> usize {
+    find_hash(input, |digest| {
+        digest[0] == 0 && digest[1] == 0 && digest[2] == 0
+    })
+}
+
+fn find_hash(input: AocInput, pred: impl Fn(Output<Md5>) -> bool) -> usize {
     let input = input.single_line().into_bytes();
     let mut hasher = Md5::new();
     hasher.update(&input);
     for i in 0..100000000 {
-        if test_hash(hasher.clone(), i) {
+        if pred(test_hash(hasher.clone(), i)) {
             return i;
         }
     }
@@ -22,15 +35,10 @@ fn part1(input: AocInput) -> usize {
     panic!();
 }
 
-fn part2(input: AocInput) -> usize {
-    input.map(|_| 0).sum()
-}
-
-fn test_hash(mut base: Md5, suffix: usize) -> bool {
+fn test_hash(mut base: Md5, suffix: usize) -> Output<Md5> {
     let suffix = suffix.to_string().into_bytes();
     base.update(&suffix);
-    let digest = base.finalize();
-    digest[0] == 0 && digest[1] == 0 && digest[2] < 0x10
+    base.finalize()
 }
 
 #[cfg(test)]
@@ -40,10 +48,5 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(609043, part1(AocInput::from_sample()));
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(0, part2(AocInput::from_sample()));
     }
 }
