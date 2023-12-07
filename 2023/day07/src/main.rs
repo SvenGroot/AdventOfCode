e583c95249c5c9df21584bf1de2dfc52d88068e8
Sven Groot <sgroot@gmail.com>
2023-12-07 11:19:37 -0800

    2023 day 7 part 2

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -12,17 +12,21 @@ fn main() {
 // Order the hands by strength, where place in the order is rank, multiply rank by bid to get the
 // total winnings.
 fn part1(input: AocInput) -> usize {
-    let mut hands = input.parsed::<Hand>().into_vec();
+    play_game(input.parsed::<Hand>().into_vec())
+}
+
+// The same, but treat J as a joker instead of a jack.
+fn part2(input: AocInput) -> usize {
+    play_game(input.map(|line| Hand::from_str(&line, true)).into_vec())
+}
+
+fn play_game(mut hands: Vec<Hand>) -> usize {
     hands.sort();
     hands
         .iter()
         .enumerate()
         .map(|(rank, hand)| (rank + 1) * hand.bid)
         .sum()
-}
-
-fn part2(input: AocInput) -> usize {
-    input.map(|_| 0).sum()
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -32,10 +36,8 @@ struct Hand {
     hand_type: HandType,
 }
 
-impl FromStr for Hand {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Hand {
+    fn from_str(s: &str, use_joker: bool) -> Self {
         let (cards, bid) = s.split_once(' ').unwrap();
         let cards: Vec<_> = cards
             .bytes()
@@ -43,19 +45,33 @@ impl FromStr for Hand {
                 b'A' => 14,
                 b'K' => 13,
                 b'Q' => 12,
-                b'J' => 11,
+                b'J' => {
+                    if use_joker {
+                        1
+                    } else {
+                        11
+                    }
+                }
                 b'T' => 10,
                 _ => card - b'0',
             })
             .collect();
 
         let bid = bid.parse().unwrap();
-        let hand_type = HandType::from_cards(&cards);
-        Ok(Self {
+        let hand_type = HandType::from_cards(&cards, use_joker);
+        Self {
             cards,
             bid,
             hand_type,
-        })
+        }
+    }
+}
+
+impl FromStr for Hand {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::from_str(s, false))
     }
 }
 
@@ -88,9 +104,20 @@ enum HandType {
 }
 
 impl HandType {
-    fn from_cards(cards: &[u8]) -> Self {
+    fn from_cards(cards: &[u8], use_joker: bool) -> Self {
+        let joker_count = if use_joker {
+            cards.iter().filter(|&&c| c == 1).count()
+        } else {
+            0
+        };
+
+        if joker_count == 5 {
+            return Self::FiveOfAKind;
+        }
+
         let mut counts: Vec<_> = cards
             .iter()
+            .filter(|&&c| !use_joker || c != 1)
             .fold(HashMap::<u8, usize>::new(), |mut map, item| {
                 map.entry(*item)
                     .and_modify(|count| *count += 1)
@@ -103,6 +130,7 @@ impl HandType {
 
         counts.sort();
         counts.reverse();
+        counts[0] += joker_count;
         match counts.as_slice() {
             [5, ..] => Self::FiveOfAKind,
             [4, ..] => Self::FourOfAKind,
@@ -126,6 +154,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(AocInput::from_sample()));
+        assert_eq!(5905, part2(AocInput::from_sample()));
     }
 }
