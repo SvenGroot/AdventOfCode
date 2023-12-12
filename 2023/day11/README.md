f223710bc1926b2f00560cd334dfb0f45a6c1976
Sven Groot <svgroot@microsoft.com>
2023-12-12 12:54:50 -0800

    2023 day 11 part 2

--- 2023/day11/README.md
+++ 2023/day11/README.md
@@ -1,3 +1,7 @@
 # Day 11
 
 [Puzzle description.](https://adventofcode.com/2023/day/11)
+
+I absolutely called that part 2 would be the same but with a much larger expansion value, which
+made it obviously that trying to physically expand the grid in memory would not be feasible. Plus,
+it would've been harder anyway than just counting rows and columns multiple times.
--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -11,29 +11,33 @@ use aoc::{
 
 fn main() {
     println!("Part 1: {}", part1(AocInput::from_input()));
-    println!("Part 2: {}", part2(AocInput::from_input()));
+    println!("Part 2: {}", part2(AocInput::from_input(), 999999));
 }
 
 // Double empty rows and columns, then get shortest path between each pair of galaxies.
 fn part1(input: AocInput) -> usize {
-    let universe = Universe::from_input(input);
+    let universe = Universe::from_input(input, 1);
     universe.shortest_paths_sum()
 }
 
-fn part2(input: AocInput) -> usize {
-    input.map(|_| 0).sum()
+// Same, but expand empty rows and columns 1,000,000 times.
+// N.B. This is a parameter because the test uses 100 times.
+fn part2(input: AocInput, expansion_amount: usize) -> usize {
+    let universe = Universe::from_input(input, expansion_amount);
+    universe.shortest_paths_sum()
 }
 
 // Just casually modelling the entire universe.
 #[derive(Debug)]
 struct Universe {
     space: Grid<Space>,
-    double_rows: HashSet<usize>,
-    double_cols: HashSet<usize>,
+    expanded_rows: HashSet<usize>,
+    expanded_cols: HashSet<usize>,
+    expansion_amount: usize,
 }
 
 impl Universe {
-    fn from_input(input: AocInput) -> Self {
+    fn from_input(input: AocInput, expansion_amount: usize) -> Self {
         let space = GridBuilder::from_input(input)
             .map(|_, value| match value {
                 b'#' => Space::Galaxy,
@@ -42,7 +46,7 @@ impl Universe {
             })
             .build();
 
-        let double_rows = space
+        let expanded_rows = space
             .rows()
             .enumerate()
             .filter_map(|(row_index, row)| {
@@ -52,7 +56,7 @@ impl Universe {
             })
             .collect();
 
-        let double_cols = space
+        let expanded_cols = space
             .cols()
             .enumerate()
             .filter_map(|(col_index, mut col)| {
@@ -62,8 +66,9 @@ impl Universe {
 
         Self {
             space,
-            double_rows,
-            double_cols,
+            expanded_rows,
+            expanded_cols,
+            expansion_amount,
         }
     }
 
@@ -83,16 +88,16 @@ impl Universe {
     fn shortest_path(&self, galaxy1: Point, galaxy2: Point) -> usize {
         // Shortest path is just manhattan distance, but we have to account for expanded rows and
         // columns.
-        Self::path_length(galaxy1.row(), galaxy2.row(), &self.double_rows)
-            + Self::path_length(galaxy1.col(), galaxy2.col(), &self.double_cols)
+        self.path_length(galaxy1.row(), galaxy2.row(), &self.expanded_rows)
+            + self.path_length(galaxy1.col(), galaxy2.col(), &self.expanded_cols)
     }
 
-    fn path_length(start: usize, end: usize, doubles: &HashSet<usize>) -> usize {
+    fn path_length(&self, start: usize, end: usize, expanded: &HashSet<usize>) -> usize {
         let min = start.min(end);
         let max = start.max(end);
-        let double_count = (min..max).filter(|value| doubles.contains(value)).count();
+        let expanded_count = (min..max).filter(|value| expanded.contains(value)).count();
 
-        (max - min) + double_count
+        (max - min) + (expanded_count * self.expansion_amount)
     }
 }
 
@@ -113,6 +118,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(AocInput::from_sample()));
+        assert_eq!(8410, part2(AocInput::from_sample(), 99));
     }
 }
